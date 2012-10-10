49b468303fe90bd73a82839545b78bac3d7d7b34
Beena Shetty <beena.s@gvocom.com>
2012-10-10 14:51:47 +0530

    first commit

---
+++ remote.rb
@@ -0,0 +1,57 @@
+require "rubygems"
+require "net/http"
+require "net/https"
+require "uri"
+require 'cgi'
+
+module Remote
+  def set_header(cookie=nil,referer=nil)
+    {"Cookie" => cookie , "Referer" => referer ,"Content-Type" => "application/x-www-form-urlencoded","User-Agent" => "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20091020 Ubuntu/9.10 (karmic) Firefox/3.5.3 GTB7.0" }
+  end
+
+  def connect(url)
+    uri = URI.parse url
+    http = Net::HTTP.new(uri.host,uri.port)
+ 
+    if uri.scheme == "https"
+      http.use_ssl = true
+      http.ca_path = "/etc/ssl/certs"
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    end
+    cookie,referer = [nil,nil]
+    self.url = url
+    if url =='http://160by2.com'
+      self.cookie,self.referer,self.response,self.http= nil, nil,nil,http
+    else
+      self.cookie,self.referer,self.response,self.http = fetch(http,'/content/index.html',set_header(cookie,referer))
+    end
+
+  end
+
+  def fetch(http,path,header,data=nil,method=:get,limit=10)
+    raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+    cookie ||= header['Cookie']
+    referer ||= header['Referer']
+    if method == :get
+      response = http.get(path,header.delete_if {|i,j| j.nil? })
+    else
+      response = http.post(path,data,header.delete_if {|i,j| j.nil? })
+      cookie ||= response['set-cookie']
+    end
+    case response.code
+    when   /2\d{2}/
+      self.cookie,self.referer, self.response, self.http = cookie,referer,response,http
+      return [self.cookie,self.referer, self.response, self.http]
+    when  /3\d{2}/
+
+      v =self.url.split('//')[1]
+      reg = /http:\/\/#{v}\/(.+)/
+      cookie,referer,response,http = fetch(http,("/"+response['location'].match(reg)[1]),set_header(cookie,(self.url+path)),limit-1)
+      self.cookie,self.referer, self.response, self.http = cookie,referer,response,http
+      return [cookie,referer,response,http]
+    else
+      return "HTTP Error"
+    end
+  end
+
+end
---
+++ sms.rb
@@ -0,0 +1,40 @@
+class SMS
+  include Remote
+  attr_accessor :http
+  attr_accessor :cookie
+  attr_accessor :response
+  attr_accessor :referer
+  attr_accessor :url
+
+  def initialize(url)
+    connect(url)
+  end
+
+  def login(username, password)
+    new_http = http
+    data = 'username='+username+'&password='+password+'&Submit=Login'
+    if url == "http://160by2.com"
+      action = "/re-login"
+    else
+      action ='/Login1.action'
+    end
+    cookie,referer,response,http = fetch(new_http,action,set_header(cookie,referer),data,:post)
+  end
+
+  def send_message(action, mobile_no, message)
+    if message.length > 140
+      p "Message should be only 140 characters"
+    else
+      message = CGI.escape(message)
+      if url == "http://160by2.com"
+        action_name = "/SendSMSAction"
+        sms_data = 'action1='+action+'&mobile1='+mobile_no+'&msg1='+message
+      else
+        action_name ='/quicksms.action'
+        sms_data = 'HiddenAction=instantsms&Action='+action+'&MobNo='+mobile_no+'&textArea='+message
+      end
+      fetch(http,action_name,set_header(cookie,referer),sms_data,:post)
+    end
+  end
+end
+
